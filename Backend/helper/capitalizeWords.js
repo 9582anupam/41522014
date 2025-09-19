b380be555221025a99aad6c54901f9861eb1a7d0
Anupam <9582anupamk@gmail.com>
2025-09-19 14:45:39 +0530

    inital setup

--- Backend/helper/capitalizeWords.js
+++
@@ -1,8 +0,0 @@
-export const capitalizeWords = (str) => {
-    return str
-        .split(" ") // Split the string by spaces
-        .map(
-            (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-        ) // Capitalize the first letter
-        .join(" "); // Rejoin the words with spaces
-};
